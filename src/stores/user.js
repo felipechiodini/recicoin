e27c382b211730cf14d929b89dd3681011bfa0d2
Felipe Bona <felipechiodinibona@hotmail.com>
2024-07-24 22:22:03 -0300

    starting

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,12 +1,59 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import Router from '@/router'
+import { request } from '@/js/api.js'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const useUserStore = defineStore('user', {
+  state: () => {
+    return {
+      user: null,
+      token: null,
+      menus: null
+    }
+  },
+  getters: () => {
+    return {
+      initialLetters() {
+        try {
+          const firstLetters = this.user?.name.split(' ').map(i => i.charAt(0))
+          return firstLetters[0] + firstLetters.pop()
+        } catch (error) {
+          return ''        
+        }
+      },
+      getUser: (state) => {
+        return state.user
+      },
+      menus: (state) => {
+        return state.menus || []
+      },
+      isLoggedin: (state) => {
+        return state.token !== null
+      },
+      getToken: (state) => {
+        return state.token
+      },
+      permissions: (state) => {
+        return state.permissions
+      }
+    }
+  },
+  actions: {
+    setToken(token) {
+      this.token = token
+    },
+    setUser(user) {
+      this.user = user
+    },
+    setMenus(menus) {
+      this.menus = menus
+    },
+    logout(callback) {
+      request().post('auth/logout').then(() => {
+        localStorage.clear()
+        Router.push({ name: 'auth.login' })
+        callback(false)
+      })
+    }
+  },
+  persist: true
 })
