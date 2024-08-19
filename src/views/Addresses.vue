4335ca6b19b3c09034c89432f1e284d9b5a40ec7
Felipe Bona <felipechiodinibona@hotmail.com>
2024-08-19 12:33:10 -0300

    ajusts

--- src/views/Addresses.vue
+++ src/views/Addresses.vue
@@ -3,8 +3,14 @@
   <div class="p-3">
     <h5>Meus Endereços</h5>
     <div class="d-flex flex-column gap-3">
-      <div class="border p-3" v-for="(address, key) in [1,2,3,4,5,6,7,8,9,10]" :key="key">
-        {{ address }}
+      <div class="border p-3" v-for="(address, key) in addresses" :key="key">
+        <div class="d-flex flex-column">
+          <span>{{ address.cep }}</span>
+          <span>{{ address.street }}</span>
+          <span>{{ address.neiborhood }}</span>
+          <span>{{ address.city }}</span>
+          <span>{{ address.state }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -16,6 +22,24 @@ import Header from '@/components/Header.vue'
 export default {
   components: {
     Header
+  },
+  data: () => {
+    return {
+      loading: false,
+      addresses: []
+    }
+  },
+  mounted() {
+    this.load()
+  },
+  methods: {
+    load() {
+      this.loading = true
+      api.get('address')
+        .then(({ data }) => this.addresses = data.addresses)
+        .catch(err => console.log('deu erro', err))
+        .finally(() => this.loading = false)
+    }
   }
 }
 </script>
