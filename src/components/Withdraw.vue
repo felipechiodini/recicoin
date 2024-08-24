5373b40616ca159ee6c2cc40c818da0d4a70f2b9
Felipe Bona <felipechiodinibona@hotmail.com>
2024-08-24 11:50:04 -0300

    ajustes

--- src/components/Withdraw.vue
+++ src/components/Withdraw.vue
@@ -1,27 +1,33 @@
 <template>
-  <div class="model p-3" :class="{ 'show': modelValue }">
-    <Button @click="close()" class="mb-3">
-      <i class="pi pi-angle-left"></i>
-    </Button>
-    <h1>Regatar valor</h1>
-    <small>Informe o valor que deseja regatar</small>
-    <InputText style="height: 70px; font-size: 2rem;" type="text" v-model="value" />
-    <div class="d-flex justify-content-around my-3">
-      <Button size="small" @click="addValue(b.value)" v-for="b in buttons" :key="b">+ {{ b.value }}</Button>
+  <Modal v-model="show">
+    <div class="p-3">
+      <Button @click="close()" class="mb-3">
+        <i class="pi pi-angle-left"></i>
+      </Button>
+      <h1>Regatar valor</h1>
+      <small>Informe o valor que deseja regatar</small>
+      <InputText style="height: 70px; font-size: 2rem;" type="text" v-model="value" />
+      <div class="d-flex justify-content-around my-3">
+        <Button size="small" @click="addValue(b.value)" v-for="b in buttons" :key="b">+ {{ b.value }}</Button>
+      </div>
+      <small class="text-danger">{{ errors.get('value') }}</small>
+      <Button class="w-100 mt-3" @click="onSubmit()">
+        Resgatar
+      </Button>
     </div>
-    <Button class="w-100 mt-3" @click="onSubmit()">
-      Resgatar
-    </Button>
-  </div>
+  </Modal>
 </template>
 
 <script>
+import Modal from './Modal.vue';
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
 import Api from '@/js/api.js'
+import { ErrorBag } from '@/js/error';
 
 export default {
   components: {
+    Modal,
     InputText,
     Button
   },
@@ -35,11 +41,17 @@ export default {
       value: 0,
       show: true,
       loading: false,
+      errors: new ErrorBag(),
       buttons: [
         { value: 100 },
         { value: 300 },
         { value: 500 }
       ]
+    }
+  },
+  computed: {
+    show() {
+      return this.modelValue  
     }
   },
   methods: {
@@ -53,6 +65,7 @@ export default {
           this.close()
           this.$emit('success', data.rescue)
         })
+        .catch((errors) => this.errors.record(errors.response.data.errors))
         .finally(() => this.loading = false)
     },
     close() {
--- src/views/Withdrawals.vue
+++ src/views/Withdrawals.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-    Retiradas
+  <Header />
+  <div class="p-3">
+    <div class="d-flex align-items-end mb-3">
+      <h5>Minhas Retiradas</h5>
+    </div>
+    <div class="d-flex flex-column gap-3">
+      <div class="d-flex flex-column p-3 rounded border" v-for="(withdraw, key) in withdraws" :key="key">
+        <span>{{ withdraw.status }}</span>
+        <span>{{ withdraw.value }}</span>
+        <span>{{ withdraw.requested_at }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
+import Header from '@/components/Header.vue';
+import api from '@/js/api.js'
 
+export default {
+  components: {
+    Header
+  },
+  data: () => {
+    return {
+      withdraws: []
+    }
+  },
+  mounted() {
+    this.load()
+  },
+  methods: {
+    load() {
+      api.get('withdraw')
+        .then(({ data }) => this.withdraws = data.withdraws)
+    }
   }
+}
 </script>
